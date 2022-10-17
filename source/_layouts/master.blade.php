de524e4a62dcaa5d4be0e366c452ac6a34723180
memphis34 <115872355+memphis34@users.noreply.github.com>
2022-10-17 15:08:35 +0100

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -29,27 +29,27 @@
 
 		<link rel="stylesheet" href="./_assets/scripts/bootstrap/bootstrap.min.css">
 
-		<!-- IonIcons 
+		IonIcons 
 
 		<link rel="stylesheet" href="{{ mix('scripts/ionicons/css/ionicons.min.css', 'assets/build')}}">
 
-		<!-- Toast 
+		
 
 		<link rel="stylesheet" href="{{ mix('scripts/toast/jquery.toast.min.css', 'assets/build')}}">
 
-		<!-- OwlCarousel 
+		
 
 		<link rel="stylesheet" href="{{ mix('scripts/owlcarousel/dist/assets/owl.carousel.min.css', 'assets/build')}}>
 
 		<link rel="stylesheet" href="{{ mix('scripts/owlcarousel/dist/assets/owl.theme.default.min.css', 'assets/build')}}>
 
-		<!-- Magnific Popup 
+		
 
 		<link rel="stylesheet" href="{{ mix('scripts/magnific-popup/dist/magnific-popup.css', 'assets/build')}}">
 
 		<link rel="stylesheet" href="{{ mix('scripts/sweetalert/dist/sweetalert.css', 'assets/build')}}">
 
-		<!-- Custom style -->
+		ustom style -->
 
 		<link rel="stylesheet" href="{{ mix('css/style.css', 'assets/build')}}">
 
