92cab8a9e8e29deb33312cfc6fde6815803dd21c
memphis34 <115872355+memphis34@users.noreply.github.com>
2022-10-17 09:47:39 +0100

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -16,18 +16,177 @@
 
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
 </head>
-<body>
-    <section>
-        <header>
-            <nav>
-                <strong>{{ $page->site->title }}</strong><br>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/posts">Posts</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-            </nav>
+  <body class="skin-orange">
+		<header class="primary">
+			<div class="firstbar">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-sm-12">
+							<div class="brand">
+								<a href="index.html">
+									<img src="images/logo.png" alt="Magz Logo">
+								</a>
+							</div>						
+						</div>
+						<div class="col-md-6 col-sm-12">
+							<form class="search" autocomplete="off">
+								<div class="form-group">
+									<div class="input-group">
+										<input type="text" name="q" class="form-control" placeholder="Type something here">									
+										<div class="input-group-btn">
+											<button class="btn btn-primary"><i class="ion-search"></i></button>
+										</div>
+									</div>
+								</div>
+								<div class="help-block">
+									<div>NIGERIA FASHION MAGAZINE:</div>
+								</div>
+							</form>								
+						</div>
+						<div class="col-md-3 col-sm-12 text-right">
+							<ul class="nav-icons">
+								<li><a href="register.html"><i class="ion-person-add"></i><div>NEWSLETTER</div></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Start nav -->
+			<nav class="menu">
+				<div class="container">
+					<div class="brand">
+						<a href="#">
+							<img src="images/logo.png" alt="Magz Logo">
+						</a>
+					</div>
+					<div class="mobile-toggle">
+						<a href="#" data-toggle="menu" data-target="#menu-list"><i class="ion-navicon-round"></i></a>
+					</div>
+					<div class="mobile-toggle">
+						<a href="#" data-toggle="sidebar" data-target="#sidebar"><i class="ion-ios-arrow-left"></i></a>
+					</div>
+					<div id="menu-list">
+						<ul class="nav-list">
+							<li class="for-tablet nav-title"><a>NFM</a></li>
+							<li class="for-tablet"><a href="register.html">newsletter</a></li>
+							<li><a href="category.html">Trending</a></li>
+							<li class="dropdown magz-dropdown">
+								<a href="category.html">News <i class="ion-ios-arrow-right"></i></a>
+								<ul class="dropdown-menu">
+									<li><a href="index.html">Home</a></li>
+									<li><a href="category.html">News</a></li>
+									<li><a href="single.html">sports</a></li>
+									<li><a href="page.html">fashion</a></li>
+									<li><a href="search.html">celebrity corner</a></li>
+		</ul>
+							</li>
+							<li class="dropdown magz-dropdown magz-dropdown-megamenu"><a href="#">Top stories<i class="ion-ios-arrow-right"></i> <div class="badge">Hot</div></a>
+								<div class="dropdown-menu megamenu">
+									<div class="megamenu-inner">
+										<div class="row">
+											<div class="col-md-3">
+												<div class="row">
+													<div class="col-md-12">
+														<h2 class="megamenu-title">Trending</h2>
+													</div>
+												</div>
+												<ul class="vertical-menu">
+													<li><a href="#"><i class="ion-ios-circle-outline"></i> Mega menu is a new feature</a></li>
+													<li><a href="#"><i class="ion-ios-circle-outline"></i> This is an example</a></li>
+													<li><a href="#"><i class="ion-ios-circle-outline"></i> For a submenu item</a></li>
+													<li><a href="#"><i class="ion-ios-circle-outline"></i> You can add</a></li>
+													<li><a href="#"><i class="ion-ios-circle-outline"></i> Your own items</a></li>
+												</ul>
+											</div>
+											<div class="col-md-9">
+												<div class="row">
+													<div class="col-md-12">
+														<h2 class="megamenu-title">Featured Posts</h2>
+													</div>
+												</div>
+												<div class="row">
+													<article class="article col-md-4 mini">
+														<div class="inner">
+															<figure>
+																<a href="single.html">
+																	<img src="images/news/img10.jpg" alt="Sample Article">
+																</a>
+															</figure>
+															<div class="padding">
+																<div class="detail">
+																	<div class="time">December 10, 2016</div>
+																	<div class="category"><a href="category.html">Healthy</a></div>
+																</div>
+																<h2><a href="single.html">Duis aute irure dolor in reprehenderit in voluptate</a></h2>
+															</div>
+														</div>
+													</article>
+													<article class="article col-md-4 mini">
+														<div class="inner">
+															<figure>
+																<a href="single.html">
+																	<img src="images/news/img11.jpg" alt="Sample Article">
+																</a>
+															</figure>
+															<div class="padding">
+																<div class="detail">
+																	<div class="time">December 13, 2016</div>
+																	<div class="category"><a href="category.html">Lifestyle</a></div>
+																</div>
+																<h2><a href="single.html">Duis aute irure dolor in reprehenderit in voluptate</a></h2>
+															</div>
+														</div>
+													</article>
+													<article class="article col-md-4 mini">
+														<div class="inner">
+															<figure>
+																<a href="single.html">
+																	<img src="images/news/img14.jpg" alt="Sample Article">
+																</a>
+															</figure>
+															<div class="padding">
+																<div class="detail">
+																	<div class="time">December 14, 2016</div>
+																	<div class="category"><a href="category.html">Travel</a></div>
+																</div>
+																<h2><a href="single.html">Duis aute irure dolor in reprehenderit in voluptate</a></h2>
+															</div>
+														</div>
+													</article>
+												</div>
+											</div>
+										</div>								
+									</div>
+								</div>
+							</li>
+							<li class="dropdown magz-dropdown magz-dropdown-megamenu"><a href="#">NFM<i class="ion-ios-arrow-right"></i></a>
+								<div class="dropdown-menu megamenu">
+									<div class="megamenu-inner">
+										<div class="row">
+											<div class="col-md-3">
+												<h2 class="megamenu-title">Nigerian Fashion Magazine</h2>
+												<ul class="vertical-menu">
+													<li><a href="page.html">About us</a></li>
+													<li><a href="#">advert spaces</a></li>
+													<li><a href="#">RUNWAY</a></li>
+												</ul>
+											</div>
+										</div>
+									</div>
+								</div>
+							</li>
+							<li class="dropdown magz-dropdown"><a href="#">contact us<i class="ion-ios-arrow-right"></i></a>
+								<ul class="dropdown-menu">
+									<li><a href="#"><i class="icon ion-person"></i> facebook</a></li>
+									<li><a href="#"><i class="icon ion-heart"></i> Instagram</a></li>
+									<li><a href="#"><i class="icon ion-chatbox"></i> Twitter</a></li>
+		</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
         </header>
 
         <article>
