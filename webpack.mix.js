a543bdbbbafd3e1dd007715fab358514c0918242
memphis34 <115872355+memphis34@users.noreply.github.com>
2022-10-17 15:14:25 +0100

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,7 +16,7 @@ mix.jigsaw()
     .css('source/_assets/scripts/sweetalert/dist/sweetalert.css', 'css')
     .css('source/_assets/scripts/owlcarousel/dist/assets/owl.theme.default.min.css', 'css')
     .css('source/_assets/css/demo.css', 'css')
-    .css('source/_assets/css/skins/all.css', 'css')
+    .css('source/_assets/css/skins/all.css', 'css/skins')
     .css('source/_assets/scripts/toast/jquery.toast.min.css', 'css')
     .css('source/_assets/scripts/ionicons/css/ionicons.min.css', 'public/css')
     .options({
