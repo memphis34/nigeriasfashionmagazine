3094d510d1005e5563019a54440ce7fc4c2af5a9
memphis34 <115872355+memphis34@users.noreply.github.com>
2022-10-17 14:33:26 +0100

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,10 +7,10 @@ mix.setPublicPath('source/assets/build');
 mix.jigsaw()
     .js('source/_assets/js/main.js', 'js')
 
-    .sass('source/_assets/sass/main.scss', 'css')
-    .sass('source/_assets/sass/style.scss', 'css')
+
+    
     .css('source/_assets/css/style.css', 'css')
-    .css('source/public/css/bootstrap.min.css', 'css')
+    .css('source/_assets/scripts/bootstrap/bootstrap.min.css', 'css')
     .css('source/_assets/scripts/magnific-popup/dist/magnific-popup.css', 'css')
     .css('source/_assets/scripts/owlcarousel/dist/assets/owl.carousel.min.css', 'css')
     .css('source/_assets/scripts/sweetalert/dist/sweetalert.css', 'css')
