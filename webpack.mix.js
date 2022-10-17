344fa6aef59e22b08938fd2535a6f8df5c8922c8
memphis34 <115872355+memphis34@users.noreply.github.com>
2022-10-17 12:25:41 +0100

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,7 +6,7 @@ mix.setPublicPath('source/assets/build');
 
 mix.jigsaw()
     .js('source/_assets/js/main.js', 'js')
-    .js('source/_assets/js/', 'js')
+
     .sass('source/_assets/sass/main.scss', 'css')
     .sass('source/_assets/sass/style.scss', 'css')
     .css('source/_assets/css/style.css', 'css')
