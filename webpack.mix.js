883f16f785b64c73e729a2899f51456fccb5d8b7
memphis34 <115872355+memphis34@users.noreply.github.com>
2022-10-22 10:26:02 +0100

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,6 +6,8 @@ mix.setPublicPath('source/assets/build');
 
 mix.jigsaw()
     .js('source/_assets/js/main.js', 'js')
+    .js('source/_assets/js/demo.js','js')
+    .js('source/_assets/js/e-magz.js','js')
     .js('source/_assets/js/jquery.js', 'js')
     .js('source/_assets/js/jquery.migrate.js', 'js')
     .js('source/_assets/scripts/bootstrap/bootstrap.min.js', 'scripts/bootstrap')
