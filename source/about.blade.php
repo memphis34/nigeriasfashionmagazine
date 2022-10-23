511ffb59af5798b34cc8ea0e89b918cd979da6d1
memphis34 <115872355+memphis34@users.noreply.github.com>
2022-10-23 16:34:38 +0100

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -3,14 +3,14 @@
 @section('title', 'About')
 
 @section('content')
-    <h1>About</h1>
+    <h1>About us</h1>
 
-    <p>My name is {{ $page->owner->name }}</p>
+    <p>Nigeria Fashion Magazine was formerly Face of Fashion e magazine Africa www.fofemagafrica.com before it was transformed to Nigeria Fashion Magazine , Founded by Mr Precious Fubara, a renowned Fashion promoter in west Africa.In April 2019,the magazine was launched officially as a monthly online fashion magazine publication in Nogeria.
 
-    <h2>Links:</h2>
+   
 
-    <ul>
-        <li><a href="/twitter" target="_blank">Twitter</a></li>
-        <li><a href="/github" target="_blank">GitHub</a></li>
-    </ul>
+    
+        
+        
+    
 @endsection
