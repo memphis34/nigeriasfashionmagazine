d11431e764ddc0b1be397a6574c40beb82b7bec1
memphis34 <115872355+memphis34@users.noreply.github.com>
2022-10-23 17:31:11 +0100

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,11 +1,23 @@
 @extends('_layouts.master')
 
 @section('content')
+    <style>
+    .h1{
+     text-align: center;
+     }
+     .p{
+     text-align: center;
+     }
+     .img{
+     text-align: center;
+     }
+     </style>
     <h1>NIGERIA FASHION MAGAZINE</h1>
 
     <p>Africa's award winning fashion and style magazine spread across 35 countries in the world with channels of distribution. Office No 92c adeola Adelu victoria Island Lagos and No 6a Edinbirhh hallow Essex cm 20-2je eastern region England </p>
 
      
+
 
     
         <img src="{{ media('logo.jpg') }}" alt="ko-fi" width="300" height="260">
