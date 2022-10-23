f1d1ba56a683fb7d27b1614cdcefb78f0837c367
memphis34 <115872355+memphis34@users.noreply.github.com>
2022-10-23 17:24:48 +0100

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -8,7 +8,7 @@
      
 
     
-        <img src="{{ media('logo.jpg') }}" alt="ko-fi" width="223" height="30">
+        <img src="{{ media('logo.jpg') }}" alt="ko-fi" width="600" height="510">
 
 
     
