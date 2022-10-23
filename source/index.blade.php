66c2a601f05c0a5a43e518a20b2d1220dec228cf
memphis34 <115872355+memphis34@users.noreply.github.com>
2022-10-23 16:28:08 +0100

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -5,17 +5,15 @@
 
     <p>Africa's award winning fashion and style magazine spread across 35 countries in the world with channels of distribution. Office No 92c adeola Adelu victoria Island Lagos and No 6a Edinbirhh hallow Essex cm 20-2je eastern region England </p>
 
-    <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
+     
 
-    <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
-
-    <a href="https://ko-fi.com/Y8Y07ACJ" target="_blank" rel="noopener">
-        <img src="{{ media('kofi-button.png') }}" alt="ko-fi" width="223" height="30">
+    
+        <img src="{{ media('') }}" alt="ko-fi" width="223" height="30">
 
 
-    <h2 id="test">Asset Compilation Test</h2>
+    
 
-    <p>CSS test: <span class="test-css">text with blue border</span></p>
+    
 
-    <p>JavaScript test: <a href="#test" class="test-js">click me</a></p>
+    
 @endsection
